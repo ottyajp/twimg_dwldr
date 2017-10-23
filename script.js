90c8b44c90329a572becf77961ad8c8c3023a654
ottyajp <ottyajp@gmail.com>
2017-10-23 16:55:57 +0900

    ダウンロード部分を関数として切り出し

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 	func(request);
 });
 
+function dl(url, name){
+	const a = document.createElement('a');
+	a.download = name;
+	a.href = url;
+	a.click();
+}
+
 function func(request){
 	console.log(request);
 	if(request.match(/pbs.twimg.com\/media\//)){
@@ -10,10 +17,7 @@ function func(request){
 		const url = request.replace(/large$/,'orig');
 		const name = request.replace(/^.+\//, '').replace(/:large/, '');
 		console.log(url,name);
-		const a = document.createElement('a');
-		a.download = name;
-		a.href = url;
-		a.click();
+		dl(url, name);
 	}
 	if(request.match(/twitter.com\/.+\/status/)){
 		console.log('status');
@@ -21,9 +25,6 @@ function func(request){
 		const url = div.innerHTML.replace(/\r?\n/g, '').replace(/.+\<img data-aria-label-part="" src="/, '').replace(/".+/, '').replace(/(.+)$/, '$1:orig');
 		const name = url.replace(/^.+\//, '').replace(/:orig/, '');
 		console.log(url,name);
-		const a = document.createElement('a');
-		a.download = 'dododo';
-		a.href = url;
-		a.click();
+		dl(url, name);
 	}
 };
