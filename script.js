3211436364447bde102ef5789e8e4876e64db874
ottyajp <ottyajp@gmail.com>
2017-10-23 16:10:15 +0900

    画像単体ページとステータスページで処理を分けるように

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+	console.log('debug');
 	func(request);
 });
 
 function func(request){
-	const url = request.replace(/large$/,'orig');
-	const name = request.replace(/^.+\//, '').replace(/:large/, '');
-	const a = document.createElement('a');
-	a.download = name;
-	a.href = url;
-	a.click();
+	console.log(request);
+	if(request.match(/pbs.twimg.com\/media\//)){
+		console.log('image');
+		const url = request.replace(/large$/,'orig');
+		const name = request.replace(/^.+\//, '').replace(/:large/, '');
+		console.log(url,name);
+		const a = document.createElement('a');
+		a.download = name;
+		a.href = url;
+		a.click();
+	}
+	if(request.match(/twitter.com\/.+\/status/)){
+		console.log('status');
+	}
 };
