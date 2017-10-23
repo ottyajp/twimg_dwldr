c96757ce631b5db21ceab5b95d4022b27936c37a
ottyajp <ottyajp@gmail.com>
2017-10-23 16:56:28 +0900

    デバッグ出力を削除

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-	console.log('debug');
 	func(request);
 });
 
@@ -11,20 +10,15 @@ function dl(url, name){
 }
 
 function func(request){
-	console.log(request);
 	if(request.match(/pbs.twimg.com\/media\//)){
-		console.log('image');
 		const url = request.replace(/large$/,'orig');
 		const name = request.replace(/^.+\//, '').replace(/:large/, '');
-		console.log(url,name);
 		dl(url, name);
 	}
 	if(request.match(/twitter.com\/.+\/status/)){
-		console.log('status');
 		const div = document.querySelector('div.AdaptiveMedia-singlePhoto');
 		const url = div.innerHTML.replace(/\r?\n/g, '').replace(/.+\<img data-aria-label-part="" src="/, '').replace(/".+/, '').replace(/(.+)$/, '$1:orig');
 		const name = url.replace(/^.+\//, '').replace(/:orig/, '');
-		console.log(url,name);
 		dl(url, name);
 	}
 };
